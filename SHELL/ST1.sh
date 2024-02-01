ef04953c9671bc433d45417fc29a55c6269026f7
DHXuan <134719685+DHXuan@users.noreply.github.com>
2024-02-01 16:43:48 +0100

    Update ST1.sh

--- SHELL/ST1.sh
+++ SHELL/ST1.sh
@@ -1,5 +1,9 @@
-#!/bi<n/bash
+#!/bin/bash
 
+gcc -o yy SE1.c
+gcc -o zz SE2.c
+
+start_time=$(date +%s.%s)
 awk -F";" '
 #pour chaque ligne à partir de la deuxième puisque jignore len tête 
     NR > 1 {
@@ -21,4 +25,48 @@ awk -F";" '
             print data[order[i]]
         }
     } ' data.csv > donnees.txt
-       
+ 
+./yy
+
+./zz
+
+head -50 ResultFinalStats.txt > StatsFinales.txt 
+
+gnuplot<<EOF
+set terminal png font "helvetica, 10" size 1000,800
+set output "SU.png"
+set style data histogram
+set style histogram cluster gap 2 #je laisse une unité d'esp entre les barres 
+set style fill solid border -1 #bordure des barres
+set boxwidth 2 # determine la largeur des barres
+set key fixed right top vertical Right noreverse noenhanced autotitle nobox #configure la légende du graphique, en haut à droite du graphique un à un, placé sous la précédente
+set colorbox vertical origin screen 0.9, 0.2 size screen 0.05, 0.6 front noinvert bdefault #je configure les couleurs de la legende, ainsi que leur emplacement
+
+#inverser l'axe des y pour placer le plus de trajets en haut 
+set yrange [*:*] noreverse writeback  #définit automatiquement la plage de donnees, noreverse pour ne pas inverser les données 
+set y2range [*:*] noreverse writeback 
+set xrange [*:*] noreverse writeback
+set xtics rotate by 45 offset -3,-2 font "helvetica, 8"
+set xtics left 
+set lmargin at screen 0.1 #je regle la marge gauche 
+set bmargin at screen 0.10
+
+set datafile separator ';'
+
+#titre des etiquettes
+set xlabel "Routes ID" offset char 0, char -3
+set ylabel "Distance (km)" offset char 1, char 0 #je le decale un peu à droite 
+set title "Option -s : Distance = f(Route)"
+
+#charger les donnees depuis le fichier 
+plot 'sfinal.txt' using 2:xticlabels(1) with lines title 'Distances Max/Min (Km)' lc rgb "skyblue", '' u 4:xticlabels(1) with lines title "" lc rgb "skyblue", '' u 3 with lines title 'Distance moyenne (km)' lc rgb "#4863A0"
+EOF
+
+
+end_time=$(date +%s.%s)
+elapsed_time=$(echo "$end_time - $start_time" | bc)
+echo "Temps écoulé : $elapsed_time secondes"
+
+rm donnees.txt
+rm StatsTrajets.txt
+rm ResultFinalStats.txt     
