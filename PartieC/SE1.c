6afd055e48517ea2e52915bff83b09d847326bd6
DHXuan <134719685+DHXuan@users.noreply.github.com>
2024-02-01 15:39:21 +0100

    Add files via upload

---
+++ PartieC/SE1.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+typedef struct chainon {
+  int routeID;
+  float distance;
+  struct chainon * next;
+}
+Chainon;
+
+Chainon * creationChainon(int a, float b) {
+  Chainon * c = malloc(sizeof(Chainon));
+  if (c == NULL) {
+    exit(5);
+  }
+  c->routeID = a;
+  c->distance = b;
+  c->next = NULL;
+  return c;
+}
+
+Chainon * insertFin(Chainon * pliste, int a, float b){
+  Chainon * n = creationChainon(a, b);
+  Chainon * p1;
+  if (pliste == NULL) {
+    return n;
+  }
+  p1 = pliste;
+  while (p1->next != NULL) {
+    p1 = p1->next;
+  }
+  p1->next = n;
+  return pliste;
+}
+
+Chainon* insertDebut(Chainon* pliste, int a, float b){
+  Chainon * n = creationChainon(a, b);
+  
+  if (pliste == NULL) {
+    return n;
+  }
+  n->next = pliste;
+  pliste = n;
+  
+  return pliste;
+}
+  
+//fonction permettant de savoir le maximum
+float MaxiStep(float a, float b){
+  if (a <= b) {
+    return b;
+  }
+  return a;
+}
+//fonction permettant de savoir le minimum
+float MiniStep(float a, float b){
+  if (a >= b) {
+    return b;
+  }
+  return a;
+}
+
+//permet de faire la somme des distances
+
+/*float somme (float ){
+  float result=0;
+  for (int i=0; i<25; i++){
+    result = result + score[2*i+1];
+}
+  return result;
+}*/
+
+
+int main() {
+
+  float max = 0;
+  float min = 100000000; //on initialise le min à une valeur très grande pour pouvoir comparer avec les valeurs des distances
+  float som = 0;
+  float moy;
+  float diff;
+  int a;
+  float b;
+
+  FILE * fichier = NULL;
+  Chainon * pliste = NULL;
+
+  printf("start\n");
+
+  //ouverture du fichier
+  fichier = fopen("donnees.txt", "r");
+
+  //on récurèpe les données du fichiers et on les insère dans une liste chainée
+  while (fscanf(fichier, "%d", &a) == 1) {
+    printf("a=%d\n", a);
+
+    fscanf(fichier, "%f", &b);
+
+    printf("b=%f\n", b);
+    pliste = insertDebut(pliste, a, b);
+
+  }
+
+  if (pliste == NULL) {
+    printf("Liste chainée vide \n");
+  } else {
+    printf("ok\n");
+  }
+
+  FILE * fichier2;
+  // Ouvre le fichier.txt en mode écriture = fichier dans lequel on écrit
+  fichier2 = fopen("StatsTrajets.txt", "a");
+  // ordre des données : Route ID, distance minimum, distance maximum, distance moyenne,
+
+  // Vérifie si le fichier a été ouvert avec succès
+  if (fichier2 == NULL) {
+    printf("Impossible d'ouvrir le fichier.\n");
+    return 1;
+  }
+
+  //phase traitemanet : on calcule le maximum, le minimum, la somme des distances et la moyennede chaque route ID dans la liste chainée
+  
+  Chainon * p1;
+  
+  
+  //on compare initialise le min, le max, la som aux valeurs de la première étape du premier trajet de la liste
+  p1 = pliste;
+  int count = 0;
+  max = MaxiStep(max, p1->distance);
+  min = MiniStep(min, p1->distance);
+  som = som + p1->distance;
+  count += 1;
+      
+  while (p1 && p1->next != NULL){
+    while (p1->next == NULL || p1->routeID == p1->next->routeID) {
+       if (p1->next == NULL){
+        break;
+        }
+      p1 = p1->next; // on passe au suivant seulement si le Route ID est le même
+      max = MaxiStep(max, p1->distance);//on met à jour le max en fonction de la valeur ajoutée
+      min = MiniStep(min, p1->distance);//on met à jour le min en fonction de la valeur ajoutée
+      som = som + p1->distance; // on met à jour la somme totale
+      count += 1; // on augmente d'1 le nombre de step de la route ID
+      }
+      
+    moy = som / count; // Calcul moyenne
+    
+    diff = max - min; // Calcul de la diff
+
+    // Écriture dans le fichier
+
+    fprintf(fichier2, "%d ", p1->routeID);
+    fprintf(fichier2, "%f ", min);
+    fprintf(fichier2, "%f ", max);
+    fprintf(fichier2, "%f ", moy);
+    fprintf(fichier2, "%f\n", diff);
+    
+    p1 = p1->next; // on passse au suivant 
+    
+    //on initialise le max le min aux valeurs de la première étape du trajet suivant si seulement il existe un suivant
+    if (p1!=NULL){
+    
+    max = p1->distance;
+    min = p1->distance;
+    som = p1->distance;
+    count = 1;
+    }
+  } 
+
+  // Ferme les fichiers
+  fclose(fichier);
+  fclose(fichier2);
+
+  return 0; // Fin du programme
+}
---
+++ PartieC/SE2.c
@@ -0,0 +1,264 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+typedef struct chainon {
+  int RID; //route ID
+  float min; //minimum
+  float max; // maximum
+  float moy; //moyenne
+  float diff; //difference max-min
+  struct chainon * next; // pointe vers le prochain chainon
+}
+Trajet;
+
+Trajet* creationChainon(int a, float b, float c, float d, float e) {
+  Trajet* n = malloc(sizeof(Trajet));
+  if (n == NULL) {
+    exit(5);
+  }
+  n->RID = a;
+  n->min = b;
+  n->max = c;
+  n->moy = d;
+  n->diff = e;
+  n->next = NULL;
+  return n;
+}
+
+Trajet* insert(Trajet* t, int a, float b, float c, float d, float e) {
+  Trajet* n;
+  Trajet* p1 = t;
+  if (t == NULL) {
+    return creationChainon(a, b, c, d, e);
+  } else {
+    
+    n = creationChainon(a, b, c, d, e);
+    n->next = t;
+    return n;
+  }
+}
+
+float min2(float a, float b) {
+  if (a <= b) {
+    return a;
+  }
+  return b;
+}
+
+float min3(float a, float b, float c) {
+    return min2(min2(a, b), c);
+}
+
+float max2(float a, float b) {
+  if (a >= b) {
+    return a;
+  }
+  return b;
+}
+
+float max3(float a, float b, float c) {
+    return max2(max2(a, b), c);
+}
+
+typedef struct AVL {
+  struct AVL* fg;
+  struct AVL* fd;
+  Trajet* tr;
+  int equilibre;
+} AVL;
+
+typedef AVL* pAVL;
+
+pAVL createAVL(Trajet* t) {
+  pAVL n = malloc(sizeof(AVL));
+  if (n == NULL) {
+    exit(1);
+  }
+  n->fg = NULL;
+  n->fd = NULL;
+  n->tr = t;
+  n->equilibre = 0;
+  return n;
+}
+
+pAVL rotaGauche(pAVL a) {
+  if (a == NULL) {
+    exit(3);
+  }
+  pAVL pivot;
+  int eqa, eqp;
+  pivot = a->fd;
+  a->fd = pivot->fg;
+  pivot->fg = a;
+  eqa = a->equilibre;
+  eqp = pivot->equilibre;
+  a->equilibre = eqa - max2(eqp, 0) - 1;
+  pivot->equilibre = min3(eqa - 2, eqa + eqp - 2, eqp - 1);
+  a = pivot;
+   printf ("rota gauche faite\n");
+  return a;
+
+}
+pAVL rotaDroite(pAVL a) {
+    if (a == NULL) {
+        exit(3);
+    }
+
+    pAVL pivot;
+    int eqa, eqp;
+    pivot = a->fg;  // Utilisez le sous-arbre gauche comme pivot pour la rotation droite
+    a->fg = pivot->fd;
+    pivot->fd = a;
+    eqa = a->equilibre;
+    eqp = pivot->equilibre;
+    a->equilibre = eqa + max2(-eqp, 0) + 1;  // Inverser les opérations pour la rotation droite
+    pivot->equilibre = max3(eqa + 2, eqa + eqp + 2, eqp + 1);
+    a = pivot;
+    printf ("rota droite faite\n");
+    return a;
+}
+
+pAVL doubleRotaDroite(pAVL a) {
+  a->fg = rotaGauche(a->fg);
+  //printf("test5\n");
+  return rotaDroite(a);
+}
+
+pAVL doubleRotaGauche(pAVL a) {
+  a->fd = rotaDroite(a->fd);
+  return rotaGauche(a);
+}
+
+pAVL equilibrerAVL(pAVL a){
+  if (a->equilibre >= 2){
+    if (a->fd->equilibre >= 0){
+      return rotaGauche(a);
+    }
+    else {
+      return doubleRotaGauche(a);
+    }
+  }
+  else if (a->equilibre <= -2){
+    if (a->fg->equilibre <= 0){
+      return rotaDroite(a);
+    }
+    else{
+      return doubleRotaDroite(a);
+    }
+  }
+return a;
+}
+
+/*pAVL insertionAVL(pAVL a, Trajet* t, int* h) {
+  if (a == NULL) {
+    *h = 1;
+    return createAVL(t);
+  } else if (a->tr->diff > t->diff) {
+    a->fg = insertionAVL(a->fg, t, h);
+    *h = - *h;
+  } else if (a->tr->diff < t->diff) {
+    a->fd = insertionAVL(a->fd, t, h);
+  } else {
+    *h = 0;
+    return a;
+  }
+  if (*h != 0) {
+    a->equilibre = a->equilibre + *h;
+    if (a->equilibre == 0) {
+      *h = 0;
+    } else {
+      *h = 1;
+    }
+  }
+  return a;
+}*/
+
+pAVL ajouterAVL(pAVL a, Trajet* t, int* h){
+
+  if (a==NULL){
+    *h = 1;
+    return createAVL(t);
+  }
+  else if (a->tr->diff>=t->diff){
+    a->fg = ajouterAVL(a->fg, t, h);
+    *h = -*h;
+  }
+  else if (a->tr->diff<t->diff){
+    a->fd = ajouterAVL(a->fd, t, h);
+  }
+
+  //equilibrage  
+  if (*h!=0){
+    a->equilibre = a->equilibre + *h;
+    a = equilibrerAVL(a);
+    if (a->equilibre == 0){
+      *h = 0;
+    }
+    else {
+      *h = 1;
+    }
+
+  }
+  return a;
+}
+
+void parcoursDRG(pAVL a, FILE * f) {
+  if (a != NULL) {
+    parcoursDRG(a->fd, f);
+    fprintf(f, "%d;%f;%f;%f;%f\n", a->tr->RID, a->tr->min,  a->tr->moy, a->tr->max, a->tr->diff);
+    parcoursDRG(a->fg, f);
+  }
+}
+
+int main() {
+  FILE* fichier1;
+  FILE* fichier2;
+  fichier1 = fopen("StatsTrajets.txt", "r"); //ouverture mode lecture du fichier StatsTrajets.txt contenant les stats suivantes sur l'ensemble des trajets : Route ID, Min, Max, Moy, Diff
+  fichier2 = fopen("ResultFinalStats.txt", "a"); //ouverture en mode ajout "add" d'un nouveau fichier.
+  int a; //ID de la route
+  float b; // minimum
+  float c; // maximum
+  float d; // moyenne
+  float e; // distance max-min
+  Trajet* pliste = NULL;
+  Trajet* p1 = NULL;
+  int indice = 1;
+
+  // récup des données de StatsTrajets.txt
+while (fscanf(fichier1, "%d", &a) == 1) {
+      //printf("a = %d\n", a);
+      fscanf(fichier1, "%f", &b);
+      fscanf(fichier1, "%f", &c);
+      fscanf(fichier1, "%f", &d);
+      fscanf(fichier1, "%f", &e);
+      pliste = insert(pliste, a, b, c, d, e);
+  }
+  p1 = pliste;
+
+  //Creation d'une liste chainée de structure contenant les stats de l'ensemble des trajets. PS : dernier chainon avec que des 0: RID = 0, min = 0, max = 0, moy = 0, diff = 0.
+  fclose(fichier1); //fermeture du fichier StatsTrajets.txt
+
+  // Creation AVL
+  pAVL racine = NULL;
+  p1 = pliste;
+  int x = 0;
+  int* h = &x;
+
+  // Creation + Isertion d'un chainon AVL pour chaque trajets contenu dans la liste chainée.
+  while (p1->next != NULL) {
+    racine = ajouterAVL(racine, p1, h);
+
+    p1 = p1->next;
+  }
+
+  // Ecriture des stats des trajets triées dans l'ordre décroissant des différences de distance max-min dans ResultFinalStats.txt via parcours DRG
+  parcoursDRG(racine, fichier2);
+  fclose(fichier2); //fermeture du fichier ResultFinalStats.txt
+
+  return 0;
+}
+
+//ResultFinalStats.txt à cut les 50 premiers avant de gnuplot.
+
+
